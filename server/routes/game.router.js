34b9f037709d67bc16fb27089f1709ec504dee98
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2025-05-09 01:16:58 -0500

    bonus

--- server/routes/game.router.js
+++ server/routes/game.router.js
@@ -8,6 +8,6 @@ router.post('/start', gameController.startGame);
 router.post('/marco', gameController.notifyMarco);
 router.post('/polo', gameController.notifyPolo);
 router.post('/select-polo', gameController.selectPolo);
-router.post("/restart", gameController.restartGame); 
+router.post("/reset-game", gameController.resetGame);
 
 module.exports = router;
